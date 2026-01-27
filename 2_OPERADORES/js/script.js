556b765414ec464e5ed222f9811cfc8fd5582b1b
Arthur Lopes <arthurlopespereirasilva000@gmail.com>
2026-01-27 10:07:31 -0300

    pause aula 15

---
+++ 2_OPERADORES/js/script.js
@@ -0,0 +1,26 @@
+//1 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -112);
+
+// 2 - operações aritmeticas
+console.log(2 + 5);
+console.log(21 - 2);
+console.log(4 * 12)
+console.log(2 / 5)
+
+// 3 - Special numbers no JS
+console.log(typeof Infinity);
+
+console.log(12 * "ass");
+
+// 4 - tipo string
+console.log("Um texto");
+console.log(`Mais um texto`)
+console.log('13')
+
+console.log(typeof "Um texto");
+console.log(typeof `Mais um texto`)
+
+// 5 - Caracteres especias do js
+
