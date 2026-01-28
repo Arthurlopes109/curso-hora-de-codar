c6d2867827bda140d5d15e266ca96786047d7af3
Arthur Lopes <arthurlopespereirasilva000@gmail.com>
2026-01-28 10:24:33 -0300

    pause aula 18

--- 2_OPERADORES/js/script.js
+++ 2_OPERADORES/js/script.js
@@ -6,8 +6,8 @@ console.log(typeof -112);
 // 2 - operações aritmeticas
 console.log(2 + 5);
 console.log(21 - 2);
-console.log(4 * 12)
-console.log(2 / 5)
+console.log(4 * 12);
+console.log(2 / 5);
 
 // 3 - Special numbers no JS
 console.log(typeof Infinity);
@@ -16,11 +16,21 @@ console.log(12 * "ass");
 
 // 4 - tipo string
 console.log("Um texto");
-console.log(`Mais um texto`)
-console.log('13')
+console.log(`Mais um texto`);
+console.log('13');
 
 console.log(typeof "Um texto");
-console.log(typeof `Mais um texto`)
+console.log(typeof `Mais um texto`);
 
-// 5 - Caracteres especias do js
+// 5 - Caracteres especias nas strings do js
+console.log("Texto em \nduas linhas");
 
+console.log("Espaçamento de \ttab");
+
+// 6 concatenação de string (união de textos)
+console.log("oi," + " Tudo" + " bem");
+
+console.log(`Testando` + ` com` + ` crase!`);
+
+// 7 interpolaçâo de textos(template string)
+console.log(`O resultado da expressão 5 + (4 x 12) é ${5 + (4 * 12)}`);
