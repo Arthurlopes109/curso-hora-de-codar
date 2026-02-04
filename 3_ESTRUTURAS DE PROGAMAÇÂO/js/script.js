97c53bb1fbb969fc61a93fdebb727fc19a55ac38
Arthur Lopes <arthurlopespereirasilva000@gmail.com>
2026-02-04 10:11:39 -0300

    start estruturas de progamação

---
+++ 3_ESTRUTURAS DE PROGAMAÇÂO/js/script.js
@@ -0,0 +1,40 @@
+// 1 - variáves
+let nome = "Arhur";
+
+console.log(nome);
+
+nome = "Arthur lopes";
+
+console.log(nome);
+
+const idade = 19;
+
+console.log(idade);
+// idade = 20 não se pode fazer um re-atribuição
+//de valor em uma constande
+
+// 2 - Mais sobre variáveis
+// simbolos não permitido no começo de uma variável
+// let 2testes ="var invalida"
+// let @teste = "var invalida"
+
+let a = 10,
+ b = 20, 
+ c = 30;
+
+ console.log(a,b,c);
+
+const Nomecompleto = "Arthur Lopes";
+
+const nomecompleto = "arthur lopes";
+
+console.log(Nomecompleto);
+
+console.log(nomecompleto);
+
+// simbolos permitidos no começo de uma variável
+let _teste = "ok"
+
+let $teste = "ok"
+
+console.log(_teste, $teste)
