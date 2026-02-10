cd97adbbba312c789270cab1df2f39cabb4aed2b
Arthur Lopes <arthurlopespereirasilva000@gmail.com>
2026-02-10 10:08:51 -0300

    pause aula 37

--- 3_ESTRUTURAS DE PROGAMAÇÂO/js/script.js
+++ 3_ESTRUTURAS DE PROGAMAÇÂO/js/script.js
@@ -33,8 +33,37 @@ console.log(Nomecompleto);
 console.log(nomecompleto);
 
 // simbolos permitidos no começo de uma variável
-let _teste = "ok"
+let _teste = "ok";
 
-let $teste = "ok"
+let $teste = "ok";
 
-console.log(_teste, $teste)
+console.log(_teste, $teste);
+
+/* 3 prompt
+const age = prompt("Digite a sua idade ?");
+
+console.log(`Você tem ${age} anos de idade.`)*/
+
+/* 4 alert
+
+alert("testando");
+
+const z = 2;
+
+alert(`O número é ${z}`);*/
+
+// 5 objeto math
+console.log(Math.max(4, 5, 7, 32, 45)); // encontra o maior
+
+console.log(Math.min(34, 12, 345 , 6545, 1)); // encontra o menor
+
+console.log(Math.floor(5.14)); // arredonda um número para baixo
+
+console.log(Math.ceil(5.14)) // arredonda para um número para cima
+
+// 6 objeto console
+console.log("exibe uma mensagem");
+
+console.error("ERRO!");
+
+console.warn("AVISO!");
