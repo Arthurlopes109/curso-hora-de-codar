732d07f1b9bc9f2771e189e137cd81cd45ffcf0e
Arthur Lopes <arthurlopespereirasilva000@gmail.com>
2026-02-17 10:30:21 -0300

    pause aula 45

--- 3_ESTRUTURAS DE PROGAMAÇÂO/js/script.js
+++ 3_ESTRUTURAS DE PROGAMAÇÂO/js/script.js
@@ -1,129 +1,163 @@
-// 1 - variáves
-let nome = "Arhur";
+// // 1 - variáves
+// let nome = "Arhur";
 
-console.log(nome);
+// console.log(nome);
 
-nome = "Arthur lopes";
+// nome = "Arthur lopes";
 
-console.log(nome);
+// console.log(nome);
 
-const idade = 19;
+// const idade = 19;
 
-console.log(idade);
-// idade = 20 não se pode fazer um re-atribuição
-//de valor em uma constande
+// console.log(idade);
+// // idade = 20 não se pode fazer um re-atribuição
+// //de valor em uma constande
 
-// 2 - Mais sobre variáveis
-// simbolos não permitido no começo de uma variável
-// let 2testes ="var invalida"
-// let @teste = "var invalida"
+// // 2 - Mais sobre variáveis
+// // simbolos não permitido no começo de uma variável
+// // let 2testes ="var invalida"
+// // let @teste = "var invalida"
 
-let a = 10,
- b = 20, 
- c = 30;
+// let a = 10,
+//  b = 20, 
+//  c = 30;
 
- console.log(a,b,c);
+//  console.log(a,b,c);
 
-const Nomecompleto = "Arthur Lopes";
+// const Nomecompleto = "Arthur Lopes";
 
-const nomecompleto = "arthur lopes";
+// const nomecompleto = "arthur lopes";
 
-console.log(Nomecompleto);
+// console.log(Nomecompleto);
 
-console.log(nomecompleto);
+// console.log(nomecompleto);
 
-// simbolos permitidos no começo de uma variável
-let _teste = "ok";
+// // simbolos permitidos no começo de uma variável
+// let _teste = "ok";
 
-let $teste = "ok";
+// let $teste = "ok";
 
-console.log(_teste, $teste);
+// console.log(_teste, $teste);
 
-/* 3 prompt
-const age = prompt("Digite a sua idade ?");
+// /* 3 prompt
+// const age = prompt("Digite a sua idade ?");
 
-console.log(`Você tem ${age} anos de idade.`)*/
+// console.log(`Você tem ${age} anos de idade.`)*/
 
-/* 4 alert
+// /* 4 alert
 
-alert("testando");
+// alert("testando");
 
-const z = 2;
+// const z = 2;
 
-alert(`O número é ${z}`);*/
+// alert(`O número é ${z}`);*/
 
-// 5 objeto math
-console.log(Math.max(4, 5, 7, 32, 45)); // encontra o maior
+// // 5 objeto math
+// console.log(Math.max(4, 5, 7, 32, 45)); // encontra o maior
 
-console.log(Math.min(34, 12, 345 , 6545, 1)); // encontra o menor
+// console.log(Math.min(34, 12, 345 , 6545, 1)); // encontra o menor
 
-console.log(Math.floor(5.14)); // arredonda um número para baixo
+// console.log(Math.floor(5.14)); // arredonda um número para baixo
 
-console.log(Math.ceil(5.14)) // arredonda para um número para cima
+// console.log(Math.ceil(5.14)) // arredonda para um número para cima
 
-// 6 objeto console
-// console.log("exibe uma mensagem");
+// // 6 objeto console
+// // console.log("exibe uma mensagem");
 
-// console.error("ERRO!");
+// // console.error("ERRO!");
 
-// console.warn("AVISO!");
+// // console.warn("AVISO!");
 
-// 7 - if
-const m = 10;
+// // 7 - if
+// const m = 10;
 
-if (m > 5) {
-    console.log("M é maior que 5");
+// if (m > 5) {
+//     console.log("M é maior que 5");
+// }
+
+// const user = "João";
+
+// if(user === "João") {
+//     console.log("Entrada aceita.");
+// }
+
+// if(user === "Maria"){
+//     console.log("Olá, Maria");
+// }
+
+// console.log(user === "João", user === "Maria")
+
+// // 8 - else
+// const loggerdin = false
+
+// if(loggerdin){
+//     console.log("Está autenticado!");
+// } else {
+//     console.log("Não está autenticado!");
+// }
+
+// const q = 10
+// const w = 15
+
+// if(q > 5 && w > 20) {
+//     console.log("Números mais altos");  
+// } else {
+//     console.log("Os números não são mais altos!");
+// }
+
+// // 9 else if
+
+// if(1 > 2){
+//     console.log("teste");
+// } else if(2 > 3){
+//     console.log("teste 2");
+// }else if(5 > 1){
+//     console.log("Agora sim")
+// }
+
+// const username = "Arthur"
+// const userage = 19
+
+// if(username === "Carlos"){
+//     console.log("Bem vindo Carlos!");
+// }else if(username === "Arthur" && userage === 19){
+//     console.log("Olá Arthur, você tem 19 anos!");
+// }else{
+//     console.log("AVISO!, seus dados não foram aceitos.")
+// }
+
+// 10 - while
+// let p = 0;
+
+// while(p < 5){
+//     console.log(`Repetindo ${p}`);
+//     p = p + 1;
+// }
+
+// loop infinito
+
+// let x = 10
+
+// while(x > 5){
+//     console.log(`Repetindo ${5}`)
+// }
+
+// 11 - do while
+
+// let o = 10
+
+// do{
+//     console.log(`Valor de o: ${o}`);
+//     o--;
+// }while(o > 0);
+
+// 12 - for
+
+for(let t = 0; t < 10; t++) {
+    console.log("Repetindo algo...")
 }
 
-const user = "João";
-
-if(user === "João") {
-    console.log("Entrada aceita.");
+let r = 10
+for(r; r > 0; r--){
+ console.log(`R está diminuindo ${r}`)
 }
-
-if(user === "Maria"){
-    console.log("Olá, Maria");
-}
-
-console.log(user === "João", user === "Maria")
-
-// 8 - else
-const loggerdin = false
-
-if(loggerdin){
-    console.log("Está autenticado!");
-} else {
-    console.log("Não está autenticado!");
-}
-
-const q = 10
-const w = 15
-
-if(q > 5 && w > 20) {
-    console.log("Números mais altos");  
-} else {
-    console.log("Os números não são mais altos!");
-}
-
-// 9 else if
-
-if(1 > 2){
-    console.log("teste");
-} else if(2 > 3){
-    console.log("teste 2");
-}else if(5 > 1){
-    console.log("Agora sim")
-}
-
-const username = "Arthur"
-const userage = 19
-
-if(username === "Carlos"){
-    console.log("Bem vindo Carlos!");
-}else if(username === "Arthur" && userage === 19){
-    console.log("Olá Arthur, você tem 19 anos!");
-}else{
-    console.log("AVISO!, seus dados não foram aceitos.")
-}
-
-// 10 estruturas de repetção
