5ef26a5beed7924c6f4db8755fc82e24ddfc89e8
Arthur Lopes <arthurlopespereirasilva000@gmail.com>
2026-02-25 09:50:58 -0300

    pause  aula 56

--- 4_FUNÇÕES/js/script.js
+++ 4_FUNÇÕES/js/script.js
@@ -5,12 +5,14 @@ function minhaFuncao() {
 
 minhaFuncao();
 minhaFuncao(); // reaproveitamento
+//fim
 
 const minhaFuncaoEmVariavel = function() {
     console.log("Função em variável");
 }
 
 minhaFuncaoEmVariavel();
+//fim
 
 function funcaoComParametro(texto) {
     console.log(`Imprimindo: ${texto}`);
@@ -19,3 +21,52 @@ function funcaoComParametro(texto) {
 funcaoComParametro("Olá");
 
 funcaoComParametro("Outra função");
+//fim
+
+
+// 2 return
+
+const a = 10
+const b = 20
+const c = 30
+const d = 40
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+const resultado = soma(a, b);
+
+console.log(resultado);
+
+console.log(soma(c, d));
+
+// Par ou impar
+
+const A = 12
+const B = 24
+const C = 54
+const D = 967
+
+function parOUimpar(n1){
+    if(n1 % 2 === 0) {
+        return `${n1} Par`;
+    } else {
+        return `${n1} Impar`;
+    }
+}
+
+console.log(parOUimpar(D));
+
+// 3 escopo da função
+
+let y = 10 
+
+function testandoEscopo() {
+    let y = 20
+    console.log(`Y dentro da função é ${y}`)
+}
+
+console.log(`Y fora da função é ${y}`)
+
+testandoEscopo()
